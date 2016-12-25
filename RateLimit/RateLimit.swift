20452230fcfd98059320d42c19c386008d306594
Brad Miller <brad@cynicalpeak.com>
2016-12-24 19:26:55 -0700

    converted to swift 3

--- RateLimit/RateLimit.swift
+++ RateLimit/RateLimit.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-public class RateLimit: NSObject {
+open class RateLimit: NSObject {
 
-    public class func execute(name name: String, limit: NSTimeInterval, @noescape block: Void -> ()) -> Bool {
+    open class func execute(name: String, limit: TimeInterval, block: (Void) -> ()) -> Bool {
         if shouldExecute(name: name, limit: limit) {
             block()
 			return true
@@ -19,14 +19,14 @@ public class RateLimit: NSObject {
         return false
     }
 
-    public class func resetLimitForName(name: String) {
-        dispatch_sync(queue) {
-            dictionary.removeValueForKey(name)
+    open class func resetLimitForName(_ name: String) {
+        _ = queue.sync {
+            dictionary.removeValue(forKey: name)
         }
     }
 
-    public class func resetAllLimits() {
-        dispatch_sync(queue) {
+    open class func resetAllLimits() {
+        queue.sync {
             dictionary.removeAll()
         }
     }
@@ -34,9 +34,9 @@ public class RateLimit: NSObject {
 
     // MARK: - Private
 
-    static let queue = dispatch_queue_create("com.samsoffes.ratelimit", DISPATCH_QUEUE_SERIAL)
+    static let queue = DispatchQueue(label: "com.samsoffes.ratelimit", attributes: [])
 
-	static var dictionary = [String: NSDate]() {
+	static var dictionary = [String: Date]() {
 		didSet {
 			didChangeDictionary()
 		}
@@ -46,10 +46,10 @@ public class RateLimit: NSObject {
 		// Do nothing
 	}
 
-    private class func shouldExecute(name name: String, limit: NSTimeInterval) -> Bool {
+    fileprivate class func shouldExecute(name: String, limit: TimeInterval) -> Bool {
 		var should = false
 
-		dispatch_sync(queue) {
+		queue.sync {
 			// Lookup last executed
 			if let lastExecutedAt = dictionary[name] {
 				let timeInterval = lastExecutedAt.timeIntervalSinceNow
@@ -62,7 +62,7 @@ public class RateLimit: NSObject {
 
 			// Record execution any time we actually execute
             if (should) {
-                dictionary[name] = NSDate()
+                dictionary[name] = Date()
             }
 		}
 		
--- Tests/RateLimitTests.swift
+++ Tests/RateLimitTests.swift
@@ -13,15 +13,15 @@ class RateLimitTests: XCTestCase {
 
     func testLimit() {
         let name = "testLimit"
-        let limit : NSTimeInterval = 5.0
+        let limit : TimeInterval = 5.0
 
         // It should get excuted first
-        let expectation1 = expectationWithDescription("Execute 1")
+        let expectation1 = expectation(description: "Execute 1")
         var reported = RateLimit.execute(name: name, limit: limit) {
             expectation1.fulfill()
         }
         XCTAssertTrue(reported)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
 
         // Not right away after
         reported = RateLimit.execute(name: name, limit: limit) {
@@ -41,24 +41,24 @@ class RateLimitTests: XCTestCase {
 		sleep(2)
         
         // Now it should get executed (after the full interval)
-        let expectation2 = expectationWithDescription("Execute 2")
+        let expectation2 = expectation(description: "Execute 2")
         reported = RateLimit.execute(name: name, limit: limit) {
             expectation2.fulfill()
         }
         XCTAssertTrue(reported)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
     }
 
     func testResetting() {
         let name = "testResetting"
 
         // It should get excuted first
-        let expectation1 = expectationWithDescription("Execute 1")
+        let expectation1 = expectation(description: "Execute 1")
         let reported1 = RateLimit.execute(name: name, limit: 1) {
             expectation1.fulfill()
         }
         XCTAssertTrue(reported1)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
 
         // Not right away after
         let reported2 = RateLimit.execute(name: name, limit: 1) {
@@ -70,25 +70,25 @@ class RateLimitTests: XCTestCase {
         RateLimit.resetLimitForName(name)
 
         // Now it should get executed
-        let expectation2 = expectationWithDescription("Execute 2")
+        let expectation2 = expectation(description: "Execute 2")
         let reported3 = RateLimit.execute(name: name, limit: 1) {
             expectation2.fulfill()
         }
         XCTAssertTrue(reported3)
-        waitForExpectationsWithTimeout(0, handler: nil)
+        waitForExpectations(timeout: 0, handler: nil)
     }
 
 	func testResettingAll() {
         // It should get excuted first
 		let name1 = "TestResettingAll1"
-		let expecation1 = expectationWithDescription("Execute 1")
+		let expecation1 = expectation(description: "Execute 1")
 		let reported1 = RateLimit.execute(name: name1, limit: 1) {
 			expecation1.fulfill()
 		}
 		XCTAssertTrue(reported1)
 
 		let name2 = "TestResettingAll2"
-		let expecation2 = expectationWithDescription("Execute 2")
+		let expecation2 = expectation(description: "Execute 2")
 		let reported2 = RateLimit.execute(name: name2, limit: 1) {
             expecation2.fulfill()
         }
@@ -109,18 +109,18 @@ class RateLimitTests: XCTestCase {
         RateLimit.resetAllLimits()
 
         // Now it should get executed
-		let expectation3 = expectationWithDescription("Execute 3")
+		let expectation3 = expectation(description: "Execute 3")
 		let reported5 = RateLimit.execute(name: name1, limit: 1) {
             expectation3.fulfill()
         }
         XCTAssertTrue(reported5)
 
-        let expectation4 = expectationWithDescription("Execute 4")
+        let expectation4 = expectation(description: "Execute 4")
 		let reported6 = RateLimit.execute(name: name2, limit: 1) {
             expectation4.fulfill()
         }
         XCTAssertTrue(reported6)
 
-		waitForExpectationsWithTimeout(1, handler: nil)
+		waitForExpectations(timeout: 1, handler: nil)
     }
 }
